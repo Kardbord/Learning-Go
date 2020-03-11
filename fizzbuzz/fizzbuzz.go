7fab516e1ce768f8fff89b25d16c30052d6fb14c
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2020-03-11 17:31:57 -0600

    Implemented parameterized FizzBuzz

---
+++ fizzbuzz/fizzbuzz.go
@@ -0,0 +1,21 @@
+package fizzbuzz
+
+import (
+	"errors"
+	"fmt"
+)
+
+func Fizzbuzz(nums []int) error {
+	if nums == nil { return errors.New("passed in argument is nil") }
+	for _, i := range nums {
+		fizz := (i % 3) == 0
+		buzz := (i % 5) == 0
+		switch {
+			case fizz && buzz: fmt.Println("FizzBuzz")
+			case fizz:         fmt.Println("Fizz")
+			case buzz:				 fmt.Println("Buzz")
+			default:           fmt.Println(i)
+		}
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	fib "scratch/fibonacci"
+	"scratch/fizzbuzz"
 	"strconv"
 )
 
@@ -17,6 +18,23 @@ type program struct {
 }
 
 var knownPrograms = map[string]program {
+	"fizzbuzz": {
+		"fizzbuzz",
+		"The 'fizzbuzz' program optionally takes 3 base-10 integer arguments.\n" +
+			"    $1 : The beginning of a range; must be less than $2\n" +
+			"    $2 : The end of a range; must be greater than $1\n" +
+			"    $3 : The size of the step to be taken while generating the range; must be greater than 0\n" +
+			"If no arguments are provided, arguments are assumed to be as follows:\n" +
+			"    $1 = 1\n" +
+			"    $2 = 100\n" +
+			"    $3 = 1\n" +
+			"For each of these arguments, one of four things will occur.\n" +
+			"    1. 'Fizz' will be printed if the argument is evenly divisible by 3\n" +
+			"    2. 'Buzz' will be printed if the argument is evenly divisible by 5\n" +
+			"    3. 'FizzBuzz' will be printed if the argument is evenly divisible by both 3 and 5\n" +
+			"    4. The argument will be printed if it is not evenly divisible by 3 or 5",
+		runFizzBuzz,
+	},
 	"fibonacci": {
 		"fibonacci",
 		"The 'fibonacci' program takes any number of base-10 16-bit unsigned integer arguments.\n" +
@@ -28,23 +46,39 @@ var knownPrograms = map[string]program {
 // ------------------------------------- MAIN ------------------------------------- //
 
 func main() {
+	
+	printHelp := func() {
+		fmt.Println("This program takes the following as command line arguments:\n" +
+			"    $1  : a string specifying which program to run. Known programs are", getProgNames(), "\n" +
+			"    $2  : An argument to pass to the program specified in $1\n" +
+			"    ... : An argument to pass to the program specified in $1")
+	}
+	
 	if len(os.Args) < 2 {
 		fmt.Println("No arguments provided, printing help.")
-		fmt.Println("This program takes the following as command line arguments:\n" +
-			              "    $1  : a string specifying which program to run. Known programs are", getProgNames(), "\n" +
-			              "    $2  : An argument to pass to the program specified in $1\n" +
-			              "    ... : An argument to pass to the program specified in $1")
+		printHelp()
+		return
+	}
+	helpFlags := []string{"-h", "--h", "-help", "--help", "help", "h"}
+	if strListContains(os.Args[:2], helpFlags) {
+		printHelp()
 		return
 	}
 	
-	if progToRun, ok := knownPrograms[os.Args[1]]; ok {
+	if progToRun, ok := knownPrograms[os.Args[1]]; !ok {
+		// Did not find the program in our map
+		fmt.Printf("'%s' does not match any known functionality. Supported functions are %v", os.Args[1], getProgNames())
+	} else {
+		// Found the program in our map
+		if strListContains(os.Args[2:], helpFlags) {
+			fmt.Println(progToRun.helpMsg)
+			return
+		}
 		err := progToRun.runFunction()
 		if err != nil {
 			fmt.Println(err)
 			fmt.Println(progToRun.helpMsg)
 		}
-	} else {
-		fmt.Printf("'%s' does not match any known functionality. Supported functions are %v", os.Args[1], getProgNames())
 	}
 }
 
@@ -67,6 +101,27 @@ func runFib() error {
 	return nil
 }
 
+func runFizzBuzz() error {
+	fbArgs := os.Args[2:]
+	if len(fbArgs) == 0 {
+		list, err := NewSlice(1, 100, 1)
+		if err != nil { return err }
+		return fizzbuzz.Fizzbuzz(list)
+	} else if len(fbArgs) != 3 {
+		return fmt.Errorf("fizzbuzz requires exactly 0 or 3 arguments; %d arguments provided", len(fbArgs))
+	} else {
+		start, err := strconv.Atoi(fbArgs[0])
+		if err != nil { return err }
+		end, err := strconv.Atoi(fbArgs[1])
+		if err != nil { return err }
+		step, err := strconv.Atoi(fbArgs[2])
+		if err != nil { return err }
+		list, err := NewSlice(start, end, step)
+		if err != nil { return err }
+		return fizzbuzz.Fizzbuzz(list)
+	}
+}
+
 // ------------------------------------ HELPERS ----------------------------------- //
 
 func getProgNames() []string {
@@ -75,4 +130,30 @@ func getProgNames() []string {
 		progNames = append(progNames, "'" + key + "' ")
 	}
 	return progNames
+}
+
+func NewSlice(start, end, step int) ([]int, error) {
+	if step <= 0 {
+		return nil, fmt.Errorf("step size cannot be zero or negative; provided step was %d", step)
+	}
+	if start > end {
+		return nil, errors.New("'start' cannot be greater than 'end'")
+	}
+	slice := make([]int, 0, ((end - start) / step) + 1)
+	
+	for start <= end {
+		slice = append(slice, start)
+		start += step
+	}
+	
+	return slice, nil
+}
+
+func strListContains(strlist []string, keys []string) bool {
+	for _, val := range strlist {
+		for _, key := range keys {
+			if val == key { return true }
+		}
+	}
+	return false
 }
