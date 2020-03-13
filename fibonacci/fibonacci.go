b021b4129b9197f1e25dcd26891b9bd0d9ab73cd
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2020-03-13 13:38:09 -0600

    Cleaned up Fibonacci

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -3,18 +3,10 @@ package fibonacci
 // Computes the n'th fibonacci number
 func Fibonacci(n uint32) uint64 {
 	if n == 0 || n == 1 { return uint64(n) }
-	var val1 uint64 = 0
-	var val2 uint64 = 1
+	var val1, val2 uint64 = 0, 1
 	
-	var i uint32 = 2
-	for i <= n {
-		if (i % 2) == 0 {
-			val1 += val2
-		} else {
-			val2 += val1
-		}
-		i++
+	for i := uint32(2); i <= n; i++ {
+		val1, val2 = val2, val1+val2
 	}
-	if (i % 2) == 0 { return val2 }
-	return val1
+	return val2
 }
